df9e9644094a8a852523435e79982c2ccee49cd0
Saber <37632033+Saaaber@users.noreply.github.com>
2019-09-28 16:02:24 -0400

    Added results dir

--- utils/xml_maker.py
+++ utils/xml_maker.py
@@ -1,3 +1,4 @@
+import os
 import xml.etree.cElementTree as ET
 
 age_list = [
@@ -12,7 +13,7 @@ gender_list = [
     "female"
     ]
 
-def make_xml(uid, age_group, gender, extrovert, neurotic, agreeable, conscientious, _open):
+def make_xml(save_dir, uid, age_group, gender, extrovert, neurotic, agreeable, conscientious, _open):
     root = ET.Element('root')
 
     ET.SubElement(root, 'user',
@@ -26,10 +27,14 @@ def make_xml(uid, age_group, gender, extrovert, neurotic, agreeable, conscientio
         open=str(_open))
 
     data = ET.tostring(root)
+
+    file_name = str(uid) + ".xml"
+
+    file_path = os.path.join(save_dir, file_name)
     
-    with open(str(uid) + ".xml", "wb") as file_name:
+    with open(file_path, "wb") as file_name:
         file_name.write(data)
 
 
 if __name__ == "__main__":
-    make_xml('123fsgfg45', 3, 0, 1.55, 2.3, 3.7, 1.1, 0.12)
+    make_xml('results', '123fsgfg45', 3, 0, 1.55, 2.3, 3.7, 1.1, 0.12)
