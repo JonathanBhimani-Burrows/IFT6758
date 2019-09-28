1e653f6ef8e47997d59218c4062691134b414d72
Saber <37632033+Saaaber@users.noreply.github.com>
2019-09-28 17:37:30 -0400

    Changed baseline path

--- ift6758.py
+++ ift6758.py
@@ -22,7 +22,7 @@ def test():
 
     # Baseline.eval(path=profile_path)
 
-    baseline_data_path = '/data/baseline_data.csv'
+    baseline_data_path = 'data\Baseline_data.csv'
 
     data = Baseline.load_data(baseline_data_path)
     profile = Baseline.load_data(profile_path)
@@ -30,7 +30,6 @@ def test():
     for user in data['userid'].itertuple():
         make_xml(save_dir=output_path, uid=user, age_group=data['age'], gender=data['gender'], extrovert=data['ext'],
                            neurotic=data['neu'], agreeable=data['agr'], conscientious=data['con'], _open=data['ope'])
-
 
 if __name__ == '__main__':
     test()
