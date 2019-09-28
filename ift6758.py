41d13b2234b9df4b04b090f4161b55142863857d
Jonathan Bhimani-Burrows <jonathanbhimani-burrows@hotmail.com>
2019-09-28 17:39:02 -0400

    Added Data

--- ift6758.py
+++ ift6758.py
@@ -3,6 +3,7 @@ import os
 import Baseline
 import pandas as pd
 from utils.xml_maker import make_xml
+import pdb
 
 
 def test():
@@ -16,13 +17,16 @@ def test():
 
     # Baseline.eval(path=profile_path)
 
-    baseline_data_path = '/data/baseline_data.csv'
+    baseline_data_path = 'data/Baseline_data.csv'
 
     data = Baseline.load_data(baseline_data_path)
     profile = Baseline.load_data(profile_path)
 
-    for user in data['userid'].itertuple():
-        make_xml(save_dir=output_path, uid=user, age_group=data['age'], gender=data['gender'], extrovert=data['ext'],
+    # pdb.set_trace()
+
+    for user in profile.itertuple():
+        id = user['userid']
+        make_xml(save_dir=output_path, uid=id, age_group=data['age'], gender=data['gender'], extrovert=data['ext'],
                            neurotic=data['neu'], agreeable=data['agr'], conscientious=data['con'], _open=data['ope'])
 
 
