63d75ae10eac761532aeedf44acc270c21af2c9b
Fanny <fanny.salvail-berard@usherbrooke.ca>
2019-11-03 12:45:10 -0500

    debug

--- visualization.py
+++ visualization.py
@@ -55,8 +55,8 @@ def get_visualization(userids, image_data, profile, output_path):
         genders_df.append(profile[profile['userid'] == uid]['gender'])
 
     for cpt_user in range(len(genders_df)):
-        pdb.set_trace()
-        genders.append(genders_df[cpt_user][1])
+        # pdb.set_trace()
+        genders.append(genders_df[cpt_user][0])
 
 
 
